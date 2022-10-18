057f274d159449b339839353cc4c66223fe6c2f7
sooyoungkang <kang.sooyoung@hotmail.com>
2022-10-19 04:27:38 +0900

    Added /dist to .gitignore

--- dist/configs/RoutingConfig.js
+++
@@ -1,6 +0,0 @@
-export const routingControllerOptions = {
-    cors: true,
-    controllers: [`${__dirname}/../controllers/*{.ts,.js}`],
-    // defaultErrorHandler: false
-};
-//# sourceMappingURL=RoutingConfig.js.map
--- dist/configs/env.js
+++
@@ -1,9 +0,0 @@
-export const env = {
-    isDevelopment: process.env.NODE_ENV === "development",
-    isProduction: process.env.NODE_ENV === "production",
-    isTest: process.env.NODE_ENV === "test",
-    app: {
-        port: Number(process.env.PORT) || 9000,
-    },
-};
-//# sourceMappingURL=env.js.map
